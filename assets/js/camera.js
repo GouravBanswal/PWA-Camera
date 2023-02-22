0472ba5cefee7a77ed61ed86cd53fa30b6a053ea
Gourav <gouravbanswal2000@gmail.com>
2023-02-22 08:52:44 +0530

    New Camera

---
+++ assets/js/camera.js
@@ -0,0 +1,56 @@
+import {
+    startBtn,
+    captureBtn,
+    closeBtn,
+    player,
+    swtichBtn,
+    canvas,
+    restartBtn,
+    getMode
+    
+} from './constants.js' 
+// import { captureBtn, closeBtn, player, restartBtn, startBtn, swtichBtn } from "./constants.js"   
+
+export const openCamera = async() => {
+    if(navigator.mediaDevices){
+    
+    document.getElementById("loader").innerHTML= `Opening Camera <div class="spinner-border text-dark"></div>`
+    startBtn.setAttribute("disabled",true)
+   try {
+    const strim = await navigator.mediaDevices.getUserMedia({
+        video: {
+            facingMode: getMode()
+        },
+    })
+    player.srcObject = strim
+    
+   } catch (error) {
+    console.log(error);
+   }
+   
+    document.getElementById("loader").innerHTML=""
+    startBtn.removeAttribute("disabled")
+}else{
+    console.error("mediaDevices not supported");
+}
+   // handleCamera End
+   player.classList.remove("d-none")
+    captureBtn.classList.remove("d-none")
+    closeBtn.classList.remove("d-none")
+    startBtn.classList.add("d-none")
+    swtichBtn.classList.remove("d-none")
+ }
+ export const closeCamera = () => {
+    const tracks = player.srcObject.getVideoTracks()
+    for (let i = 0; i < tracks.length; i++) {
+     tracks[i].stop()
+    }
+    canvas.classList.add("d-none")
+
+    restartBtn.classList.add("d-none")
+    player.classList.add("d-none")
+    captureBtn.classList.add("d-none")
+    closeBtn.classList.add("d-none")
+    startBtn.classList.remove("d-none")
+    swtichBtn.classList.add("d-none")
+ }
